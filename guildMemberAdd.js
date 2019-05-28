02ab531ce19fba82c97e30709bcabd0b5a7a316c
jowbly <36288609+jowbly@users.noreply.github.com>
2019-05-28 03:02:12 -0300

    [FIX] Delete guildMemberAdd.js

--- guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, member) => {
-   
-   member.guild.channels.get(config.TICKET_CHANNELID).send("``👤`` Seja muito bem vindo ao nosso Discord, "+member.user.toString()+", contamos atualmente com ``"+member.guild.members.size+"`` jogadores no Discord!")
-  
-};
